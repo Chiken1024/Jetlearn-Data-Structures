9c7e3243d772334bc011840b922fff0329067f16
Chiken1024 <martinmarinauk@gmail.com>
2025-11-09 18:08:25 +0100

    Add files via upload

--- insertion_sort_recursive.py
+++ insertion_sort_recursive.py
@@ -11,5 +11,22 @@ def isr(arr: list[int], i: int = 1) -> list[int]:
   if i + 1 < len(arr): return isr(arr, i + 1)
   else: return arr
 
+def risr(arr: list[int], i: int = -1) -> list[int]:
+  if i < 0: i = len(arr) - 2
+  
+  key: int = arr[i]
+  j: int = len(arr) - i + 1
+
+  print(j, len(arr), arr[j])
+  while j <= len(arr) and key > arr[j]:
+    print(arr, i)
+    arr[j - 1] = arr[j]
+    j -= 1
+
+  arr[j + 1] = key
+
+  if i > 0: return risr(arr, i - 1)
+  else: return arr
+
 arr: list[int] = [-16, -20, 7, -5, 13, 0]
-print(isr(arr))
+print(risr(arr))
